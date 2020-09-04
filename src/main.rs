94c1af8f2fe9091b6a0861482442837033cac5cb
Owen Salter <owen@devosmium.xyz>
2020-09-04 11:22:10 -0400

    fmt

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,12 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-
-  HttpServer::new(|| {
-    App::new()
-      .route("/", web::get().to(index))
-  })
-  .bind("localhost:8080")?
-  .run()
-  .await
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind("localhost:8080")?
+        .run()
+        .await
 }
 
 async fn index() -> impl Responder {
-  HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body("Hello world!")
 }
