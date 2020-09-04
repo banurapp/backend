97a15ab62a0447559becf539e81349a664088bf7
Owen Salter <owen@devosmium.xyz>
2020-09-04 10:38:03 -0400

    add basic webserver

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,25 @@
 /*!
 * Backend for Banur
 */
-fn main() {
-    println!("Hello, world!");
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+/**
+ * Defines the main server for the backend
+ */
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+
+  HttpServer::new(|| {
+    App::new()
+      .route("/", web::get().to(index))
+  })
+  .bind("localhost:8080")?
+  .run()
+  .await
+}
+
+async fn index() -> impl Responder {
+  HttpResponse::Ok().body("Hello world!")
 }
