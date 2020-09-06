eefe170ac1d7ad2066d0aa84d73c3197b88ff310
Owen Salter <owen@devosmium.xyz>
2020-09-05 20:50:03 -0400

    fmt

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@
 * Backend for Banur
 */
 
-use std::sync::Arc;
 use std::io;
+use std::sync::Arc;
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, Error};
+use actix_web::{web, App, Error, HttpResponse, HttpServer, Responder};
 use juniper::http::graphiql::graphiql_source;
 use juniper::http::GraphQLRequest;
 
@@ -22,15 +22,15 @@ use crate::schema::{create_schema, Schema};
 async fn main() -> std::io::Result<()> {
     let schema = std::sync::Arc::new(create_schema());
     HttpServer::new(move || {
-      App::new()
-        .data(schema.clone())
-        .route("/", web::get().to(index))
-        .service(web::resource("/graphql").route(web::post().to(graphql)))
-        .service(web::resource("/graphiql").route(web::get().to(graphiql)))
+        App::new()
+            .data(schema.clone())
+            .route("/", web::get().to(index))
+            .service(web::resource("/graphql").route(web::post().to(graphql)))
+            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
     })
-        .bind("localhost:8080")?
-        .run()
-        .await
+    .bind("localhost:8080")?
+    .run()
+    .await
 }
 
 async fn index() -> impl Responder {
@@ -38,15 +38,20 @@ async fn index() -> impl Responder {
 }
 
 async fn graphiql() -> HttpResponse {
-  let html = graphiql_source("http://127.0.0.0:8080/graphql");
-  HttpResponse::Ok().body(html)
+    let html = graphiql_source("http://127.0.0.0:8080/graphql");
+    HttpResponse::Ok().body(html)
 }
 
-async fn graphql(st: web::Data<Arc<Schema>>, data: web::Json<GraphQLRequest>) -> Result<HttpResponse, Error> {
-  let user = web::block(move || {
-    let res = data.execute(&st, &schema::Context {} );
-    Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
-  })
-  .await?;
-  Ok(HttpResponse::Ok().content_type("application/json").body(user))
+async fn graphql(
+    st: web::Data<Arc<Schema>>,
+    data: web::Json<GraphQLRequest>,
+) -> Result<HttpResponse, Error> {
+    let user = web::block(move || {
+        let res = data.execute(&st, &schema::Context {});
+        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+    })
+    .await?;
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(user))
 }
--- src/schema.rs
+++ src/schema.rs
@@ -1,26 +1,26 @@
-use juniper::{FieldResult};
+use juniper::FieldResult;
 
 use uuid::Uuid;
 
 #[derive(juniper::GraphQLEnum, Clone)]
 enum UserType {
-  Admin,
-  User
+    Admin,
+    User,
 }
 
 #[derive(juniper::GraphQLObject)]
-#[graphql(description="A user within Banur")]
+#[graphql(description = "A user within Banur")]
 struct User {
-  id: String,
-  email: String,
-  user_type: UserType
+    id: String,
+    email: String,
+    user_type: UserType,
 }
 
 #[derive(juniper::GraphQLInputObject)]
-#[graphql(description="A new user to insert")]
+#[graphql(description = "A new user to insert")]
 struct NewUser {
-  email: String,
-  user_type: UserType
+    email: String,
+    user_type: UserType,
 }
 
 pub struct Context {}
@@ -30,25 +30,33 @@ impl juniper::Context for Context {}
 pub struct Query;
 #[juniper::object(Context = Context)]
 impl Query {
-  fn apiVersion() -> &str {
-    "0.1"
-  }
+    fn apiVersion() -> &str {
+        "0.1"
+    }
 
-  fn user(context: &Context, id: String) -> FieldResult<User> {
-    Ok(User { id: Uuid::new_v4().to_string(), email: String::from("test@test.xyz"), user_type: UserType::User})
-  }
+    fn user(context: &Context, id: String) -> FieldResult<User> {
+        Ok(User {
+            id: Uuid::new_v4().to_string(),
+            email: String::from("test@test.xyz"),
+            user_type: UserType::User,
+        })
+    }
 }
 
 pub struct Mutation;
 #[juniper::object(Context = Context)]
 impl Mutation {
-  fn createUser(context: &Context, new_user: NewUser) -> FieldResult<User> {
-    Ok(User { id: Uuid::new_v4().to_string(), email: new_user.email.clone(), user_type: new_user.user_type.clone() })
-  }
+    fn createUser(context: &Context, new_user: NewUser) -> FieldResult<User> {
+        Ok(User {
+            id: Uuid::new_v4().to_string(),
+            email: new_user.email.clone(),
+            user_type: new_user.user_type.clone(),
+        })
+    }
 }
 
 pub type Schema = juniper::RootNode<'static, Query, Mutation>;
 
 pub fn create_schema() -> Schema {
-  Schema::new(Query {}, Mutation {})
+    Schema::new(Query {}, Mutation {})
 }
