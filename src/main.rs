e4da9a1f469f87a30728e94646cac04152b298a3
Owen Salter <owen@devosmium.xyz>
2020-09-05 20:49:17 -0400

    add graphql routes to main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,16 @@
 * Backend for Banur
 */
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Arc;
+use std::io;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, Error};
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+
+/// Contains the GraphQL schema
+mod schema;
+use crate::schema::{create_schema, Schema};
 
 /**
  * Defines the main server for the backend
@@ -11,7 +20,14 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+    let schema = std::sync::Arc::new(create_schema());
+    HttpServer::new(move || {
+      App::new()
+        .data(schema.clone())
+        .route("/", web::get().to(index))
+        .service(web::resource("/graphql").route(web::post().to(graphql)))
+        .service(web::resource("/graphiql").route(web::get().to(graphiql)))
+    })
         .bind("localhost:8080")?
         .run()
         .await
@@ -19,4 +35,18 @@ async fn main() -> std::io::Result<()> {
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+async fn graphiql() -> HttpResponse {
+  let html = graphiql_source("http://127.0.0.0:8080/graphql");
+  HttpResponse::Ok().body(html)
+}
+
+async fn graphql(st: web::Data<Arc<Schema>>, data: web::Json<GraphQLRequest>) -> Result<HttpResponse, Error> {
+  let user = web::block(move || {
+    let res = data.execute(&st, &schema::Context {} );
+    Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+  })
+  .await?;
+  Ok(HttpResponse::Ok().content_type("application/json").body(user))
 }
